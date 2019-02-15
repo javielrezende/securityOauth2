138ca0976bc7ceb753cbb6787e14f3c868a1c760
Roger Rezende <javielrezende@gmail.com>
2019-02-15 14:24:54 -0200

    Implementa ouath2 - Em funcionamento

---
+++ src/main/java/com/javielrezende/securityoauth2/config/AutorizationServerConfig.java
@@ -0,0 +1,60 @@
+package com.javielrezende.securityoauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AutorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    /**
+     * Responsavel por pegar o usuario e senha
+     */
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    /**
+     * Autoriza a aplicacao cliente a utilizar o authentication server
+     * @param clients da acesso a metodos para configuracao
+     * @throws Exception
+     *
+     * secret == senha
+     * accessTokenValiditySeconds == 30 minutos == 1800 segundos
+     */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("angular")
+                .secret("@ngul@r0")
+                .scopes("read", "write")
+                .authorizedGrantTypes("password")
+                .accessTokenValiditySeconds(1800);
+    }
+
+    /**
+     * Utilizado para armazenar o token
+     * @param endpoints
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
+
+}
---
+++ src/main/java/com/javielrezende/securityoauth2/config/ResourceServerConfig.java
@@ -0,0 +1,53 @@
+package com.javielrezende.securityoauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ROLE");
+    }
+
+    /**
+     *
+     * @param http libera métodos para implementação
+     * @throws Exception
+     *
+     * .anyRequest().authenticated() TODAS AS SOLICITACOES PRECISAM ESTAR AUTENTICADAS
+     * .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) NAO SERA MANTIDO ESTADO DE SESSAO (APP SEM SESSAO)
+     * .csrf().disable()
+     */
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable();
+    }
+
+    /**
+     *
+     * @param resources método que mantém o servidor sem estado (sem sessao)
+     * @throws Exception
+     */
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.stateless(true);
+    }
+}
